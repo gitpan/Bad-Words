c22062e40ce4766b4ddc5685e2e81427f44e2f34
Michael Robinton <michael@bizsystems.com>
2014-01-27 20:03:32 +0000

    Import of MIKER/Bad-Words-0.07 from CPAN.
    gitpan-cpan-distribution: Bad-Words
    gitpan-cpan-version:      0.07
    gitpan-cpan-path:         MIKER/Bad-Words-0.07.tar.gz
    gitpan-cpan-author:       MIKER
    gitpan-cpan-maturity:     released

--- Words.pm
+++ Words.pm
@@ -4,7 +4,7 @@ use strict;
 
 use vars qw($VERSION);
 
-$VERSION = do { my @r = (q$Revision: 0.06 $ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
+$VERSION = do { my @r = (q$Revision: 0.07 $ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
 
 =head1 NAME
 
